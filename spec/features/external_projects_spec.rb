feca9df9019b3f74c2ba587893e59c8e3b04b986
emmanuelaaron <emmanuelisabirye9@gmail.com>
2020-10-07 13:59:31 +0300

    adds a test for showing a specific project

--- spec/features/external_projects_spec.rb
+++ spec/features/external_projects_spec.rb
@@ -15,4 +15,39 @@ RSpec.describe 'External projects', type: :feature do
     click_on 'Create Project'
     expect(page).to have_content('External projects')
   end
+
+  scenario 'displaying all projects', js: true do
+    visit new_user_path
+    fill_in 'User name', with: 'Emmanuel'
+    fill_in 'Email', with: 'emmanuel@gmail.com'
+    click_on 'signup'
+    visit login_path
+    fill_in 'User name', with: 'Emmanuel'
+    click_on 'Login'
+    click_link 'External projects'
+    click_link 'Add Project'
+    fill_in 'project_name', with: 'React'
+    fill_in 'Number of days', with: 6
+    click_on 'Create Project'
+    click_link 'External projects'
+    expect(page).to have_content('React')
+  end
+
+  scenario 'displaying a specific project', js: true do
+    visit new_user_path
+    fill_in 'User name', with: 'Emmanuel'
+    fill_in 'Email', with: 'emmanuel@gmail.com'
+    click_on 'signup'
+    visit login_path
+    fill_in 'User name', with: 'Emmanuel'
+    click_on 'Login'
+    click_link 'External projects'
+    click_link 'Add Project'
+    fill_in 'project_name', with: 'React'
+    fill_in 'Number of days', with: 6
+    click_on 'Create Project'
+    click_link 'External projects'
+    click_link 'React'
+    expect(page).to have_content('6 days')
+  end
 end
